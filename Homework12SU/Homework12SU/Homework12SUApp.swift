524ddbef515b9395a9bfc647ff5f021ff8953e62
RuslanWorkGit <lulka758@gmail.com>
2025-01-08 12:35:57 +0200

    add project

---
+++ Homework12SU/Homework12SU/Homework12SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework12SUApp.swift
+//  Homework12SU
+//
+//  Created by Ruslan Liulka on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Homework12SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Homework12UK/Homework12UK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework12UK
+//
+//  Created by Ruslan Liulka on 08.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
